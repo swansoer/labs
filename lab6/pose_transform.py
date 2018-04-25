ab6d508c42ba6b1bf25f3a79bfbbe8e3065030cc
Eric Swanson <erswanso@microsoft.com>
2018-04-25 14:55:28 -0700

    Code for labs 6 and 7

--- lab6/pose_transform.py
+++ lab6/pose_transform.py
@@ -7,36 +7,72 @@ This is starter code for Lab 6 on Coordinate Frame transforms.
 
 import asyncio
 import cozmo
-import numpy
+import numpy as np
 from cozmo.util import degrees
+import time
 
+# def get_relative_pose(object_pose, reference_frame_pose):
+
+    # #theta = object_pose.rotation.angle_z.radians + reference_frame_pose.rotation.angle_z.radians
+    # #x = object_pose.position.x + reference_frame_pose.position.x
+    # #y = object_pose.position.y + reference_frame_pose.position.y
+    # #z = object_pose.position.z + reference_frame_pose.position.z
+    # #return cozmo.util.Pose(x,y,z,angle_z=cozmo.util.radians(theta))
+    
+    # #theta = object_pose.rotation.angle_z.radians - reference_frame_pose.rotation.angle_z.radians
+    # #transform = np.matrix([[np.cos(theta), -1*np.sin(theta) ,0, object_pose.position.x],[np.sin(theta), np.cos(theta), 0, object_pose.position.y],[0, 0, 1, object_pose.position.z],[0, 0, 0, 1]])
+    # #object = np.matrix([[reference_frame_pose.position.x],[reference_frame_pose.position.y],[reference_frame_pose.position.z],[0]])
+    # #out = transform * object
+    
+    
+    # #theta = reference_frame_pose.rotation.angle_z.radians - object_pose.rotation.angle_z.radians
+    # theta = object_pose.rotation.angle_z.radians - reference_frame_pose.rotation.angle_z.radians
+    # transform = np.matrix([[np.cos(theta), np.sin(theta) ,0, reference_frame_pose.position.x],[-1*np.sin(theta), np.cos(theta), 0, reference_frame_pose.position.y],[0, 0, 1, reference_frame_pose.position.z],[0, 0, 0, 1]])
+    # object = np.matrix([[object_pose.position.x],[object_pose.position.y],[object_pose.position.z],[0]])
+    # out = transform * object
+    
+    # return cozmo.util.Pose(out[0],out[1],out[2],angle_z=cozmo.util.radians(theta))
+    
+
+    
 def get_relative_pose(object_pose, reference_frame_pose):
-	# ####
-	# TODO: Implement computation of the relative frame using numpy.
-	# Try to derive the equations yourself and verify by looking at
-	# the books or slides before implementing.
-	# ####
-	return None
 
+    theta_0_1 = reference_frame_pose.rotation.angle_z.radians
+    p_01 = reference_frame_pose.position
+    theta_0_2 = object_pose.rotation.angle_z.radians
+    p_02 = object_pose.position
+    transform_0_1 = np.matrix([[np.cos(theta_0_1), -1*np.sin(theta_0_1) ,0, p_01.x],[np.sin(theta_0_1), np.cos(theta_0_1), 0, p_01.y],[0, 0, 1, p_01.z],[0, 0, 0, 1]])
+    transform_0_2 = np.matrix([[np.cos(theta_0_2), -1*np.sin(theta_0_2) ,0, p_02.x],[np.sin(theta_0_2), np.cos(theta_0_2), 0, p_02.y],[0, 0, 1, p_02.z],[0, 0, 0, 1]])
+    transform_1_2 = np.linalg.inv(transform_0_1)*transform_0_2
+    
+    return cozmo.util.Pose(transform_1_2.item(0,3),transform_1_2.item(1,3),transform_1_2.item(2,3),angle_z=cozmo.util.radians(theta_0_2-theta_0_1))
+    
+    
 def find_relative_cube_pose(robot: cozmo.robot.Robot):
-	'''Looks for a cube while sitting still, prints the pose of the detected cube
-	in world coordinate frame and relative to the robot coordinate frame.'''
+    '''Looks for a cube while sitting still, prints the pose of the detected cube
+    in world coordinate frame and relative to the robot coordinate frame.'''
 
-	robot.move_lift(-3)
-	robot.set_head_angle(degrees(0)).wait_for_completed()
-	cube = None
+    robot.move_lift(-3)
+    robot.set_head_angle(degrees(0)).wait_for_completed()
+    cube = None
 
-	while True:
-		try:
-			cube = robot.world.wait_for_observed_light_cube(timeout=30)
-			if cube:
-				print("Robot pose: %s" % robot.pose)
-				print("Cube pose: %s" % cube.pose)
-				print("Cube pose in the robot coordinate frame: %s" % get_relative_pose(cube.pose, robot.pose))
-		except asyncio.TimeoutError:
-			print("Didn't find a cube")
+    try:
+        while True:
+            try:
+                cube = robot.world.wait_for_observed_light_cube(timeout=30)
+                if cube:
+                    print("Robot pose: %s" % robot.pose)
+                    print("Cube pose: %s" % cube.pose)
+                    pose1 = robot.pose.define_pose_relative_this(cube.pose)
+                    print("Cube pose in the robot coordinate frame: %s" % pose)
 
+            except asyncio.TimeoutError:
+                print("Didn't find a cube")
+
+    except KeyboardInterrupt:
+        print("")
+        print("Exit requested by user")
 
 if __name__ == '__main__':
 
-	cozmo.run_program(find_relative_cube_pose)
+    cozmo.run_program(find_relative_cube_pose)
--- lab7/move_relative_to_cube.py
+++ lab7/move_relative_to_cube.py
@@ -12,35 +12,45 @@ import time
 import sys
 
 from odometry import cozmo_go_to_pose
+from odometry import my_go_to_pose1
+from odometry import my_go_to_pose2
+from odometry import my_go_to_pose3
 sys.path.insert(0, '../lab6')
 from pose_transform import get_relative_pose
 
 def move_relative_to_cube(robot: cozmo.robot.Robot):
-	'''Looks for a cube while sitting still, when a cube is detected it 
-	moves the robot to a given pose relative to the detected cube pose.'''
+    '''Looks for a cube while sitting still, when a cube is detected it 
+    moves the robot to a given pose relative to the detected cube pose.'''
 
-	robot.move_lift(-3)
-	robot.set_head_angle(degrees(0)).wait_for_completed()
-	cube = None
+    robot.move_lift(-3)
+    robot.set_head_angle(degrees(0)).wait_for_completed()
+    cube = None
+    relative_pose = None
+    
+    while cube is None:
+        try:
+            cube = robot.world.wait_for_observed_light_cube(timeout=30)
+            if cube:
+                relative_pose = get_relative_pose(cube.pose, robot.pose)
+                print("Found a cube, pose in the robot coordinate frame: %s" % relative_pose)
+        except asyncio.TimeoutError:
+            print("Didn't find a cube")
 
-	while cube is None:
-		try:
-			cube = robot.world.wait_for_observed_light_cube(timeout=30)
-			if cube:
-				print("Found a cube, pose in the robot coordinate frame: %s" % get_relative_pose(cube.pose, robot.pose))
-		except asyncio.TimeoutError:
-			print("Didn't find a cube")
-
-	desired_pose_relative_to_cube = Pose(0, 100, 0, angle_z=degrees(90))
-
-	# ####
-	# TODO: Make the robot move to the given desired_pose_relative_to_cube.
-	# Use the get_relative_pose function your implemented to determine the
-	# desired robot pose relative to the robot's current pose and then use
-	# one of the go_to_pose functions you implemented in Lab 6.
-	# ####
+    desired_pose_relative_to_cube = Pose(0*math.sin(cube.pose.rotation.angle_z.radians), 100*math.cos(cube.pose.rotation.angle_z.radians), 0, angle_z=degrees(90))
+    final_pose = cube.pose + desired_pose_relative_to_cube
+    print("Found a point, pose in the robot coordinate frame: %s" % final_pose)
+    relative_pose = get_relative_pose(final_pose, robot.pose)
+    print("Found a relative pose, pose in the robot coordinate frame: %s" % relative_pose)
+    my_go_to_pose3(robot, relative_pose.position.x, relative_pose.position.y, relative_pose.rotation.angle_z.degrees)
+    
+    # ####
+    # TODO: Make the robot move to the given desired_pose_relative_to_cube.
+    # Use the get_relative_pose function your implemented to determine the
+    # desired robot pose relative to the robot's current pose and then use
+    # one of the go_to_pose functions you implemented in Lab 6.
+    # ####
 
 
 if __name__ == '__main__':
 
-	cozmo.run_program(move_relative_to_cube)
+    cozmo.run_program(move_relative_to_cube)
